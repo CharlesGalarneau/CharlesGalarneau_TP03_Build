f7840ad4c3208b62f02302a69bc06b78e50c8471
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-04-29 10:59:56 -0400

    Travail Animation, et interface
    L'Interface avait des problèmes j'ai du la refaire, j'ai travail sur l'Animation des personnages

---
+++ Assets/script/PlayerStat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    float Hp;
+    float Attack;
+    float Defence;
+    float mana;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
