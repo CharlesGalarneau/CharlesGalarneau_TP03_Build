a0e95fc57f65bed855be66122ce4cdc001ceb52f
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-05-05 10:51:39 -0400

    personnage et déplacement
    déplacement des personnages n'est toujours pas fonctionnel

--- Assets/script/Ennemies.cs
+++ Assets/script/Ennemies.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Ennemies : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+
+    NavMeshAgent agent;
+
+    int hitpoints;
+
     void Start()
     {
-        
+        // La quantité de tir du joueur pour tué le zombie (entre 2 et 3)
+        //hitpoints = Random.Range(2, 4);
+
+      
     }
 
-    // Update is called once per frame
-    void Update()
+    // Le zombie se fait assigné sa cible (le joueur)
+    public void SetTarget(Transform t)
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        target = t;
+
+        // À toutes les secondes, le zombie ajustera la position de sa cible
+        InvokeRepeating("UpdateDestination", 0.1f, 1f);
+
+        // À toutes les 0.5s, le zombie vérifie si le joueur est a proximité (si oui, c'est game over)
+        InvokeRepeating("PlayerProximityCheck", 0.1f, 0.5f);
+        UpdateDestination();
+       
+    }
+
+    void UpdateDestination()
+    {
+        // Si le jeu est finit, on ne fait rien
+        //if (GameManager.instance.isGameOver)
+            //return;
+
+        // Ajuster la cible
+        agent.SetDestination(target.position);
+    }
+
+    void PlayerProximityCheck()
+    {
+        // Si le jeu est finit, on ne fait rien
+        if (GameManager.instance.isGameOver)
+            return;
+
+        // Récupère tous les colliders à proximité
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.4f);
+
+        foreach (var item in colliders)
+        {
+            // Si l'un des collider est celui du joueur, il meurt
+            if (item.name == "Player")
+                FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+
+    // Zombie s'est fait tiré
+    public void Shot()
+    {
+        hitpoints--;
+
+        if (hitpoints < 1)
+            Die();
+    }
+
+    // Le zombie meurt
+    public void Die()
+    {
+        CancelInvoke("UpdateDestination");
+        Destroy(gameObject);
     }
 }
--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,19 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class GameManager : MonoBehaviour
 {
 
+
+    public static GameManager instance;
+    const float spawnZoneX = 6f;
+    const float spawnZoneY = 0.6f;
+    const float spawnZoneZ = 6f;
+    public GameObject prefabSkeleton;
+    public Transform player;
+    public Transform spawnLocation;
+
+    
+    public GameObject txtGameOver;
+    float spawnInterval = 5f;
+
+    public bool isGameOver = false; //{ get; private set; }=false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawner());
+        instance = this;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        
+        Time.timeScale = 0f;
+    }
+
+
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            // Déterminer la position du zombie
+            Vector3 location = new Vector3(Random.Range(-spawnZoneX, spawnZoneX), spawnZoneY, Random.Range(-spawnZoneZ, spawnZoneZ));
+
+            // Un indicateur circulaire apparaitra à cette position pour que le joueur puisse s'en éloigner
+            Vector3 indicatorLocation = location;
+            indicatorLocation.y = 0.03f;
+            spawnLocation.position = indicatorLocation;
+
+            // Attendre un léger interval avant de le faire spawn
+            yield return new WaitForSeconds(spawnInterval);
+
+            // Spawn du zombie
+            GameObject obj = Instantiate(prefabSkeleton, location, Quaternion.identity).gameObject;
+
+            // Dire au zombie de pourchasser le joueur
+            obj.GetComponent<Ennemies>().SetTarget(player);
+
+            spawnInterval -= 0.25f;
+
+            if (spawnInterval < 1f)
+                spawnInterval = 1f;
+        }
+
     }
 }
