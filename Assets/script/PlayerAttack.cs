a0ec33f658919eff72c79cfcb7d97245746666b0
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-05-06 10:56:28 -0400

    Déplacement des ennemies
    Les ennemies se dirige vers le joueur, j'ai commencer a travailler sur les Attacks des Ennemies

--- Assets/script/PlayerAttack.cs
+++ Assets/script/PlayerAttack.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class PlayerAttacks : MonoBehaviour
 {
+    bool IsAttackingLight;
+    bool IsAttackingHeavy;
+    bool IsAttackingUnarmed;
+    bool IsattackingArmed;
+    float AttackDammage;
+    float attackSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,38 @@ public class NewBehaviourScript1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && IsAttackingLight && IsAttackingUnarmed)
+        {
+            AttacklightUnarmed();
+        }
+        if (Input.GetMouseButtonDown(0) && IsAttackingLight && IsAttackingUnarmed)
+        {
+            AttacklightUnarmed();
+        }
+        if (Input.GetMouseButtonDown(0) && IsAttackingLight && IsAttackingUnarmed)
+        {
+            AttacklightUnarmed();
+        }
+        if (Input.GetMouseButtonDown(0) && IsAttackingLight && IsAttackingUnarmed)
+        {
+            AttacklightUnarmed();
+        }
+
+    }
+    void AttacklightUnarmed()
+    {
+
+    }
+    void AttackHeavyUnarmed()
+    {
+    }
+    void AttacklightArmed()
+    {
+
+    }
+    void AttackHeavyArmed()
+    {
     }
 }
+    
+
