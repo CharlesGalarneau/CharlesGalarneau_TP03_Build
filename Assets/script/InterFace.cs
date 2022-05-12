2db445f7fa08cfe298d21647dfed512c5b8e2939
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-05-12 10:59:01 -0400

    Interface
    J'ai travaillé sur l'interface

--- Assets/script/InterFace.cs
+++ Assets/script/InterFace.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class InterFace : MonoBehaviour
+using UnityEngine.UI;
+public class Interface : MonoBehaviour
 {
+
+    public Button ManaButton;
+    public Button HealingButton;
+    public Button StaminaButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ManaButton.onClick.AddListener(ManaButtonClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ManaButtonClicked()
+    {
+
+    }
+    public void HealingButtonClicked()
+    {
+
+    }
+    public void StaminaButtonClicked()
+    {
+
     }
 }
--- Assets/script/PlayerStat.cs
+++ Assets/script/PlayerStat.cs
@@ -19,7 +19,7 @@ public class PlayerStat : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
 
     }
