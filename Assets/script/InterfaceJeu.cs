d6bf8b5feaeedcb246569c70e03b01b258082ba1
CharlesGalarneau <94868285+CharlesGalarneau@users.noreply.github.com>
2022-05-13 10:58:11 -0400

    Interface
    Début des interfaces du Menu

--- Assets/script/InterfaceJeu.cs
+++ Assets/script/InterfaceJeu.cs
@@ -12,6 +12,11 @@ public class InterfaceJeu : MonoBehaviour
     public ParticleSystem AnimationStamina;
     public ParticleSystem AnimationHealth;
     public PlayerStat playerStat;
+    public Text txtBnPotionHealing;
+    public Text txtBnPotionMana;
+    public Text txtBnPotionStamina;
+    float Timer;
+    public Text txtTimer;
     public int NbHealthPotion = 1;
     public int NbManaPotion =1;
     public int NbStaminaPotion=1 ;
@@ -27,13 +32,21 @@ public class InterfaceJeu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+            Timer += Time.deltaTime;
+            txtTimer.text = Timer.ToString();
+            txtTimer.text = string.Format("{0:0}:{1:00}",
+                   Mathf.Floor(Timer / 60),   // Minutes
+                   Mathf.Floor(Timer) % 60);  // seconds
+        txtBnPotionHealing.text = NbHealthPotion.ToString();
+        txtBnPotionMana.text = NbManaPotion.ToString();
+        txtBnPotionStamina.text = NbStaminaPotion.ToString();
     }
     public void ManaButtonClicked()
     {
         if (NbManaPotion > 0)
         {
             AnimationMana.Play();
+            NbManaPotion--;
 
         playerStat.ManaHealing();
         }
@@ -44,6 +57,7 @@ public class InterfaceJeu : MonoBehaviour
         {
             AnimationHealth.Play();
             playerStat.HealthHealing();
+            NbHealthPotion--;
         }
     }
     public void StaminaButtonClicked()
@@ -52,6 +66,7 @@ public class InterfaceJeu : MonoBehaviour
         {
             AnimationStamina.Play();
         playerStat.StaminaHealing();
+            NbStaminaPotion--;
         }
     }
 }
